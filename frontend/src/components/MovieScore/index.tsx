31ff9380d61da330399b980ddd243fb972dd9e25
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-03-12 18:15:26 -0400

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,18 @@
+import MovieStar from "components/MovieStar";
+import './styles.css';
+
+const score = 3.5;
+const count = 16;
+
+function MovieScore() {
+  return (
+    <div className="dsmovie-score-container">
+      <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : "-"}
+      </p>
+      <MovieStar />
+      <p className="dsmovie-score-count">{count} avaliações</p>
+    </div>
+  );
+}
+
+export default MovieScore
